f0886f8ba9a0794827cc19a0b3ce57c5de8a8086
EmelieAxi@hotmail.com <emelieaxi@hotmail.com>
2025-11-06 15:33:50 +0100

    Lagt till About och Contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Contact } from './contact/contact';
+import { About } from './about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Contact, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
