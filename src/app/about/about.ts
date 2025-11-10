cb6fc35a8fd7b0f8c91f24c9644f8cdcb6b1d670
EmelieAxi@hotmail.com <emelieaxi@hotmail.com>
2025-11-10 12:46:00 +0100

    css-fix och testimonials-fix

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -4,9 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   imports: [],
   templateUrl: './about.html',
-  styleUrls: ['./about.css'],
+  styleUrl: './about.css'
 })
 export class About {
   sushiImg = 'assets/food-3581341_1280.jpg';
+  
+  openVideo() {
+    window.open('https://youtube.com/shorts/iPGcSSYL9Gg?si=XufFLtwC8Ja_MRDd', '_blank');
+  }
+  
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
 }
 
